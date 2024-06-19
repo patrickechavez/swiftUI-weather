4ea81ed33d0e47b9c76bff27fe81f55f26145de8
John Patrick Echavez <patrickechavez@gmail.comm>
2024-06-19 21:38:09 +0800

    Initial commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by John Patrick Echavez on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
